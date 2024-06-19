b4e14cd9cab251c2f11b4dd839becfd9773a9a69
Ahmed8881 <buttahmed2004@gmail.com>
2024-06-19 12:43:45 +0500

    -Add Lab for regression

--- Machine Learning/4-Regression/Lab.py
+++ Machine Learning/4-Regression/Lab.py
@@ -8,3 +8,5 @@ x_train = np.array([1.0, 2.0])
 y_train = np.array([300.0, 500.0])
 print(f"x_train: {x_train}")
 print(f"y_train: {y_train}")
+#You will use m to denote the number of training examples. Numpy arrays have a .shape parameter. x_train.shape returns a python tuple with an entry for each dimension. x_train.shape[0] is the length of the array and number of examples as shown below.
+# m is the number of training examples
