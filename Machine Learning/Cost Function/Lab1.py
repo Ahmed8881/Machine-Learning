e5fd16718f004657ff23936cf67a37ef77feb309
Ahmed8881 <buttahmed2004@gmail.com>
2024-06-22 11:56:35 +0500

    Add Lab1

--- Machine Learning/Cost Function/Lab1.py
+++ Machine Learning/Cost Function/Lab1.py
@@ -1,1 +1,8 @@
-import numpy as np
+"""import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib widget
+from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+plt.style.use('./deeplearning.mplstyle')
+You would like a model which can predict housing prices given the size of the house.
+Let's use the same two data points as before the previous lab- a house with 1000 square feet sold for $300,000 and a house with 2000 square feet sold for $500,000."""
+# Data
