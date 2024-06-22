106eafd72ed4b832ab5bf2aebc6df383587df891
Ahmed8881 <buttahmed2004@gmail.com>
2024-06-22 12:32:42 +0500

    Add Lab1

--- Machine Learning/Cost Function/Lab1.py
+++ Machine Learning/Cost Function/Lab1.py
@@ -5,4 +5,7 @@ from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, sou
 plt.style.use('./deeplearning.mplstyle')
 You would like a model which can predict housing prices given the size of the house.
 Let's use the same two data points as before the previous lab- a house with 1000 square feet sold for $300,000 and a house with 2000 square feet sold for $500,000."""
+import numpy as np
 # Data
+x_train = np.array([1.0, 2.0])           #(size in 1000 square feet)
+y_train = np.array([300.0, 500.0])           #(price in 1000s of dollars)
